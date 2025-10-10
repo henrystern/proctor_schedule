28f8023bd6fde70bd2b3ccfa92a148185a2b41e4
Henry Stern <108289013+henrystern@users.noreply.github.com>
2025-10-10 11:16:08 -0400

    Setup environment

--- src/proctor_schedule/dataset.py
+++
@@ -1,7 +0,0 @@
-"""Clean and output data."""
-
-from proctor_schedule.config import PROCESSED_DATA_DIR, RAW_DATA_DIR, LOGS_DIR
-from loguru import logger
-
-if __name__ == "__main__":
-    logger.add(LOGS_DIR / "dataset.log")
--- src/proctor_schedule/plots.py
+++
@@ -1,3 +0,0 @@
-"""Generate plots."""
-
-from proctor_schedule.config import PROCESSED_DATA_DIR
